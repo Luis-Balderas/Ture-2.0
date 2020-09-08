27a41090149da30e87bef604af17df7e4e0d070d
Jose Alvarez <jose.alvarez@infometrix.co>
2020-09-07 21:41:57 -0500

    creando filtros para los eventos

--- routes/events.js
+++ routes/events.js
@@ -15,94 +15,150 @@ require('../utils/auth/strategies/jwt');
 function eventsApi(app) {
     const router = express.Router();
     app.use("/api/events", router)
-    
+
     const eventsService = new EventsService();
 
-    router.get("/", 
-    passport.authenticate('jwt', { session: false }),
-    async function(req, res, next){
-       const { tags } = req.query;
-       try{
-          const events = await eventsService.getEvents({ tags });
+    router.get("/",
+        passport.authenticate('jwt', {
+            session: false
+        }),
+        async function (req, res, next) {
+            const {
+                tags
+            } = req.query;
+            try {
+                const events = await eventsService.getEvents({
+                    tags
+                });
 
-          res.status(200).json({
-              data: events,
-              message: 'Event listed'
-          });
-       }catch(err) {
-           next(err);
-       }
-    });
+                res.status(200).json({
+                    data: events,
+                    message: 'Event listed'
+                });
+            } catch (err) {
+                next(err);
+            }
+        });
+
+    router.get('/search',
+        passport.authenticate('jwt', {
+            session: false
+        }),
+        async function (req, res, next) {
+            // const { eventId } = req.params;
+            try {
+                const event = await eventsService.filterEvents(req.query);
+                res.status(200).json({
+                    data: event,
+                    message: 'filtro'
+                });
+            } catch (err) {
+                next(err);
+            }
+        });
 
     router.get('/:eventId',
-    passport.authenticate('jwt', { session: false }),
-    validationHandler({ eventId: eventIdSchema }, 'params'),
-     async function(req, res, next){
-        const { eventId } = req.params;
-        try{
-           const event = await eventsService.getEvent({ eventId });
- 
-           res.status(200).json({
-               data: event,
-               message: 'Event retrieve'
-           });
-        }catch(err) {
-            next(err);
-        }
-     });
+        passport.authenticate('jwt', {
+            session: false
+        }),
+        validationHandler({
+            eventId: eventIdSchema
+        }, 'params'),
+        async function (req, res, next) {
+            const {
+                eventId
+            } = req.params;
+            try {
+                const event = await eventsService.getEvent({
+                    eventId
+                });
 
-     router.post("/", 
-     passport.authenticate('jwt', { session: false }),
-     validationHandler(createEventSchema),
-     async function(req, res, next){
-        const { body: event } = req;
-        try{
-           const createdEventId = await eventsService.createEvent({ event });
- 
-           res.status(200).json({
-               data: createdEventId,
-               message: 'Event created'
-           });
-        }catch(err) {
-            next(err);
-        }
-     });
+                res.status(200).json({
+                    data: event,
+                    message: 'Event retrieve'
+                });
+            } catch (err) {
+                next(err);
+            }
+        });
 
-     router.put("/:eventId", 
-     passport.authenticate('jwt', { session: false }),
-     validationHandler({ eventId: eventIdSchema }, 'params'),
-     validationHandler(updateEventSchema),
-     async function(req, res, next){
-        const { body: event } = req;
-        const { eventId } = req.params;
-        try{
-           const updatedEventId = await eventsService.updateEvent({ eventId, event });
- 
-           res.status(200).json({
-               data: updatedEventId,
-               message: 'Event updated'
-           });
-        }catch(err) {
-            next(err);
-        }
-     });
+    router.post("/",
+        passport.authenticate('jwt', {
+            session: false
+        }),
+        validationHandler(createEventSchema),
+        async function (req, res, next) {
+            const {
+                body: event
+            } = req;
+            try {
+                const createdEventId = await eventsService.createEvent({
+                    event
+                });
 
-     router.delete("/:eventId",
-     passport.authenticate('jwt', { session: false }),
-     validationHandler({ eventId: eventIdSchema }, 'params'),
-     async function(req, res, next){
-        const { eventId } = req.params;
-        try{
-           const deletedEventId = await eventsService.deleteEvent({ eventId });
- 
-           res.status(200).json({
-               data: deletedEventId,
-               message: 'Event deleted'
-           });
-        }catch(err) {
-            next(err);
-        }
-     });
+                res.status(200).json({
+                    data: createdEventId,
+                    message: 'Event created'
+                });
+            } catch (err) {
+                next(err);
+            }
+        });
+
+    router.put("/:eventId",
+        passport.authenticate('jwt', {
+            session: false
+        }),
+        validationHandler({
+            eventId: eventIdSchema
+        }, 'params'),
+        validationHandler(updateEventSchema),
+        async function (req, res, next) {
+            const {
+                body: event
+            } = req;
+            const {
+                eventId
+            } = req.params;
+            try {
+                const updatedEventId = await eventsService.updateEvent({
+                    eventId,
+                    event
+                });
+
+                res.status(200).json({
+                    data: updatedEventId,
+                    message: 'Event updated'
+                });
+            } catch (err) {
+                next(err);
+            }
+        });
+
+    router.delete("/:eventId",
+        passport.authenticate('jwt', {
+            session: false
+        }),
+        validationHandler({
+            eventId: eventIdSchema
+        }, 'params'),
+        async function (req, res, next) {
+            const {
+                eventId
+            } = req.params;
+            try {
+                const deletedEventId = await eventsService.deleteEvent({
+                    eventId
+                });
+
+                res.status(200).json({
+                    data: deletedEventId,
+                    message: 'Event deleted'
+                });
+            } catch (err) {
+                next(err);
+            }
+        });
 }
 
 module.exports = eventsApi;
--- utils/schemas/events.js
+++ utils/schemas/events.js
@@ -2,35 +2,37 @@ const joi = require('@hapi/joi');
 
 const eventIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 const eventTitleSchema = joi.string().max(80);
-const eventImagesSchema =  joi.string().uri();
+const eventImagesSchema = joi.string().uri();
 const eventDescriptionSchema = joi.string().max(750);
 const eventLocationSchema = joi.string().max(50);
 const eventPriceSchema = joi.number().max(1000000);
-const eventStartDateSchema = joi.number().min(2020).max(2020);
-const eventEndDateSchema = joi.number().min(2020).max(2020);
+const eventStartDateSchema = joi.date();
+const eventEndDateSchema = joi.date();
 const eventOcupationSchema = joi.number().max(30);
 const eventTagsSchema = joi.array().items(joi.string().max(50));
+const eventcreatedAt = joi.date();
 
-const createEventSchema = { 
-   title: eventTitleSchema,
-   images: eventImagesSchema,
-   description:eventDescriptionSchema.required(),
-   location: eventLocationSchema.required(),
-   price:eventPriceSchema.required(),
-   startDate:eventStartDateSchema.required(),
-   andDate:eventEndDateSchema.required(),
-   ocupation: eventOcupationSchema.required(),
-   tags: eventTagsSchema
+const createEventSchema = {
+    title: eventTitleSchema.required(),
+    images: eventImagesSchema.required(),
+    description: eventDescriptionSchema.required(),
+    location: eventLocationSchema.required(),
+    price: eventPriceSchema.required(),
+    startDate: eventStartDateSchema.required(),
+    andDate: eventEndDateSchema.required(),
+    ocupation: eventOcupationSchema.required(),
+    tags: eventTagsSchema,
+    createdAt: eventcreatedAt,
 };
 
 const updateEventSchema = {
     title: eventTitleSchema,
     images: eventImagesSchema,
-    description:eventDescriptionSchema,
+    description: eventDescriptionSchema,
     location: eventLocationSchema,
-    price:eventPriceSchema,
-    startDate:eventStartDateSchema,
-    andDate:eventEndDateSchema,
+    price: eventPriceSchema,
+    startDate: eventStartDateSchema,
+    andDate: eventEndDateSchema,
     ocupation: eventOcupationSchema,
     tags: eventTagsSchema
 };
